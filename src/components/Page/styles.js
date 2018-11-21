1ee98a06c4654555e848fc5c1042eced13a7b39a
Cosmo <daixianceng@gmail.com>
2018-11-21 21:51:47 +0800

    fix: 修复样式错误

--- src/components/Page/styles.js
+++ src/components/Page/styles.js
@@ -1,10 +1,10 @@
 export default theme => ({
   content: {
-    padding: [theme.spacing.unit * 6, 0],
+    padding: theme.spacing.unit * 6 + 'px 0',
   },
   [theme.breakpoints.down('sm')]: {
     content: {
-      padding: [theme.spacing.unit * 2, 0],
+      padding: theme.spacing.unit * 2 + 'px 0',
     },
   },
 });
